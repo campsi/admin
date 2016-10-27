94194927dcecad3ad5b0c2ccccad42a5608884ec
romainbessugesmeusy <romainbessuges@gmail.com>
2016-10-27 16:07:13 +0200

    prototype

---
+++ src/index.js
@@ -0,0 +1,14 @@
+var $ = require('jquery');
+var page = require('page');
+
+// fetch project name and icon
+
+// login & get access token
+
+// list collections (entities ?)
+
+// routing
+
+$.getJSON('https://www.campsi.io/api/v1/projects/afp/', function (json) {
+
+});
