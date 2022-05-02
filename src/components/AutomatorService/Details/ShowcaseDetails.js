91e01038348604144d2d6f1fff5a7b5bb9c24576
romain <romainbessuges@gmail.com>
2022-05-02 17:51:25 +0200

    Handle Showcase multiple PNG

--- src/components/AutomatorService/Details/ShowcaseDetails.js
+++ src/components/AutomatorService/Details/ShowcaseDetails.js
@@ -11,17 +11,17 @@ class ShowcaseDetails extends Component {
   }
 
   componentDidUpdate(prevProps, prevState, snapshot) {
-    if(prevProps.result !== this.props.result){
+    if (prevProps.result !== this.props.result) {
       this.updateStateFromProps();
     }
   }
 
-  updateStateFromProps(){
+  updateStateFromProps() {
     this.setState({
-      dataSource:this.props.result.map((output) => {
+      dataSource: this.props.result.map((output) => {
         return { key: output.name, ...output };
-      })
-    })
+      }),
+    });
   }
 
   getColumns = () => [
@@ -34,7 +34,11 @@ class ShowcaseDetails extends Component {
       title: "Viewport",
       dataIndex: "viewport",
       key: "viewport",
-      render: (item) => <span>{item["width"]} &times; {item["height"]} px</span>,
+      render: (item) => (
+        <span>
+          {item["width"]} &times; {item["height"]} px
+        </span>
+      ),
     },
 
     {
@@ -42,9 +46,22 @@ class ShowcaseDetails extends Component {
       dataIndex: "url",
       key: "url",
       render: (item) => {
+        if (Array.isArray(item)) {
+          return (
+            <ul>
+              {item.map((url) => (
+                <li>
+                  <a href={url}>
+                    <img src={url} width={300} alt="showcase render" />
+                  </a>
+                </li>
+              ))}
+            </ul>
+          );
+        }
         return (
           <a href={item}>
-            <img src={item} width={300} alt="showcase render"/>
+            <img src={item} width={300} alt="showcase render" />
           </a>
         );
       },
