18ceec8318a3b8948133ac437efd367d744a5c4f
Romain Gabilan <romain.gabilan@gmail.com>
2022-07-29 16:19:28 +0200

    Add notifications service crud

---
+++ src/components/NotificationsService/NotificationsService.js
@@ -0,0 +1,21 @@
+import { Route, Routes, useParams } from "react-router-dom";
+import NotificationListing from "./NotificationListing";
+import NotificationForm from "./NotificationForm";
+
+function NotificationsService(props) {
+  useParams();
+  return (
+    <Routes>
+      <Route
+        path="/:id"
+        element={<NotificationForm service={props.service} />}
+      />
+      <Route
+        path="/"
+        element={<NotificationListing service={props.service} />}
+      />
+    </Routes>
+  );
+}
+
+export default NotificationsService;
