e3655d82a04a5d5d26903bb51789a2ce3da5a501
romain <romainbessuges@gmail.com>
2022-07-30 16:13:36 +0200

    Bulk Job Creation

---
+++ src/components/AutomatorService/BulkJobCreationForm.js
@@ -0,0 +1,222 @@
+import React, { useEffect, useMemo, useState } from "react";
+import {
+  Button,
+  Form,
+  List,
+  Upload,
+  Input,
+  Progress,
+  Badge,
+  Space,
+} from "antd";
+import { UploadOutlined } from "@ant-design/icons";
+import Papa from "papaparse";
+import { generateRelationField } from "../RelationField/RelationField";
+
+/**
+ * Cooldown duration before creating subsequent jobs
+ * @type {number}
+ */
+const COOLDOWN = 500;
+
+async function sleep(timeout) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, timeout);
+  });
+}
+
+export default function BulkJobCreationForm({ api, services, form }) {
+  const [jobTemplate, setJobTemplate] = useState({});
+  const [tasks, setTasks] = useState([]);
+
+  const tasksState = JSON.stringify(tasks);
+
+  useEffect(() => {
+    /**
+     * This method will call the API to create a job using the select jobTemplate
+     * and the variables passed in the CSV string or file.
+     *
+     * @param {object} task
+     * @param {number} index
+     * @returns {Promise<void>}
+     */
+    async function createJob({ task, index }) {
+      let status = "default";
+      let error;
+      try {
+        await api.client.post(`/v1/automator/jobs:from-template`, task.payload);
+        status = "success";
+      } catch (err) {
+        status = "error";
+        error = err;
+      }
+      await sleep(COOLDOWN);
+      if (tasks.length === 0) {
+        // tasks have been cleared, we don't want to regenerate the tasks list.
+        return;
+      }
+      setTasks(
+        tasks.map((taskItem, taskIndex) => {
+          return taskIndex === index
+            ? { ...taskItem, status, error }
+            : taskItem;
+        })
+      );
+    }
+
+    // Each time the tasks array gets modified, we
+    // Get the first pending task from the tasks array
+    // and call the createJob
+    const pending = tasks.reduce((prev, task, index) => {
+      return task.status === "default" && !prev ? { task, index } : prev;
+    }, null);
+
+    if (pending) {
+      createJob(pending).then((result) => {
+        console.info("Created job", { result });
+      });
+    }
+  }, [tasksState, tasks, api.client]);
+
+  /**
+   * RelationField that points to the templates/jobs resource.
+   * @type {React.Component}
+   */
+  const JobTemplateField = useMemo(
+    () =>
+      generateRelationField({
+        resource: "jobs",
+        service: "templates",
+        labelIndex: "title",
+      }),
+    []
+  );
+
+  /**
+   * Takes a CSV row, and transform it into a task object used to
+   * update the component state (progress, errors, etc.)
+   * @param row
+   * @returns {{payload: {variables: {}, jobTemplateId}, status: string}}
+   */
+  function createTask(row) {
+    return {
+      status: "default",
+      payload: {
+        jobTemplateId: jobTemplate.id,
+        variables: jobTemplate.data.variables.reduce(
+          (variablesMap, variable, index) => {
+            return {
+              ...variablesMap,
+              [variable.technicalName]: row[index],
+            };
+          },
+          {}
+        ),
+      },
+    };
+  }
+
+  function handleFinish(formData) {
+    if (!jobTemplate) {
+      return false;
+    }
+
+    const csv = formData.csvFile?.file
+      ? formData.csvFile.file
+      : formData.csvString;
+
+    if (!csv) {
+      return false;
+    }
+
+    Papa.parse(csv, {
+      complete: (result) => setTasks(result.data.map(createTask)),
+    });
+  }
+
+  if (tasks.length > 0) {
+    return (
+      <div>
+        <List
+          header={
+            <Progress
+              percent={
+                (tasks.filter((task) => task.status === "success").length /
+                  tasks.length) *
+                100
+              }
+            />
+          }
+          bordered
+          size="small"
+          dataSource={tasks}
+          style={{ marginBottom: 16 }}
+          pagination={{
+            pageSize: 10,
+          }}
+          renderItem={(task, index) => (
+            <List.Item>
+              <Space direction="horizontal">
+                <Badge count={index + 1} status={task.status} />
+                <code>{JSON.stringify(task.payload.variables)}</code>
+              </Space>
+            </List.Item>
+          )}
+        />
+        <Button onClick={() => setTasks([])}>Clear tasks</Button>
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <Form form={form} onFinish={handleFinish}>
+        <JobTemplateField
+          api={api}
+          formData={jobTemplate.id}
+          onChange={(id, jobTemplate) => setJobTemplate(jobTemplate)}
+          services={services}
+          schema={{
+            title: "Job template",
+          }}
+          formItemProps={{
+            rules: [{ required: true, message: "A job template is required" }],
+            required: true,
+            validateStatus: jobTemplate.id ? "success" : "error",
+          }}
+        />
+        {jobTemplate.data && (
+          <List
+            style={{ marginBottom: 16 }}
+            size="small"
+            bordered
+            header={
+              <b>The selected job template requires the following variables</b>
+            }
+            dataSource={jobTemplate.data.variables}
+            renderItem={(variable) => (
+              <List.Item>
+                <label>{variable.title}</label>&nbsp;
+                <code>{variable.technicalName}</code>
+              </List.Item>
+            )}
+          />
+        )}
+        <Form.Item label="Data" name="csvString">
+          <Input.TextArea style={{ fontFamily: "monospace" }} />
+        </Form.Item>
+        <Form.Item label="CSV" name="csvFile">
+          <Upload
+            maxCount={1}
+            accept="text/csv"
+            beforeUpload={() => {
+              return false;
+            }}
+          >
+            <Button icon={<UploadOutlined />}>Load a CSV file</Button>
+          </Upload>
+        </Form.Item>
+      </Form>
+    </>
+  );
+}
