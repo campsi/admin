2389b3eec1a3a6d9eeb67a4470d8210fe7978dcb
Achalhi Ilyes <60391768+Achalhii@users.noreply.github.com>
2022-08-01 16:25:33 +0200

    update gtm response

--- src/components/AutomatorService/Details/GtmDetails.js
+++ src/components/AutomatorService/Details/GtmDetails.js
@@ -4,27 +4,14 @@ import { VerticalAlignBottomOutlined } from '@ant-design/icons';
 
 const { Item } = Descriptions;
 
-function downloadGtmJson(container) {
-  downloadFile(JSON.stringify(container.result), 'GTMContainer.json', 'application/json');
-}
-
-function downloadFile(content, fileName, contentType) {
-  const a = document.createElement('a');
-  const file = new Blob([content], { type: contentType });
-  a.href = URL.createObjectURL(file);
-  a.download = fileName;
-  a.click();
-}
-
-
-function GtmDetails(result) {
+function GtmDetails(gtmAction) {
   return (
     <Descriptions bordered column={1}>
       <Item label='GTM container'>
-        <Button
-          icon={<VerticalAlignBottomOutlined />}
-          onClick={() => downloadGtmJson(result)}
-        />
+        <a href={gtmAction.result} download>
+          <Button
+            icon={<VerticalAlignBottomOutlined />} />
+        </a>
       </Item>
     </Descriptions>
   );
