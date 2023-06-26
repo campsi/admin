5ada757673965b6ed391577c7440d1d1287e63fe
Achalhi Ilyes <60391768+Achalhii@users.noreply.github.com>
2023-06-26 16:07:00 +0200

    fmt

--- src/components/DocsService/ResourceForm.js
+++ src/components/DocsService/ResourceForm.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import PropTypes  from 'prop-types';
+import PropTypes from 'prop-types';
 import { withAppContext } from '../../App';
 import withParams from '../../utils/withParams';
 import Form from '@rjsf/antd';
