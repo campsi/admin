9da5cff1b98c8d3fbf705ed125952f09642e57ab
Achalhi Ilyes <ilyesachalhi@gmail.com>
2024-01-23 15:44:41 +0100

    improvement: add the feature flag

--- src/components/AutomatorService/AutomatorJobForm.js
+++ src/components/AutomatorService/AutomatorJobForm.js
@@ -130,6 +130,14 @@ function AutomatorJobForm({ onFinish, api }) {
               <Switch />
             </Form.Item>
             <Form.Item
+              name={['actions', 'scanner', 'newPDF']}
+              initialValue={false}
+              valuePropName="checked"
+              label="Use the new PDF"
+            >
+              <Switch />
+            </Form.Item>
+            <Form.Item
               name={['actions', 'scanner', 'followSubDomains']}
               initialValue={false}
               valuePropName="checked"
